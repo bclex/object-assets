2847271c3e1d61388f3022d6751eed16f60dc4e5
David Charlton <wahtd@icloud.com>
2018-06-16 19:23:35 -0500

    5->4

--- src.mac/ObjectManager/ObjectManager/ObjectManager/Core/Vector2Int.swift
+++ src.mac/ObjectManager/ObjectManager/ObjectManager/Core/Vector2Int.swift
@@ -7,11 +7,11 @@
 //
 
 public struct Vector2Int {
-     public let x: Int
-     public let y: Int
+    public let x: Int
+    public let y: Int
 
-     init(_ x: Int, _ y: Int) {
-         self.x = x
-         self.y = y
-     }
+    init(_ x: Int, _ y: Int) {
+        self.x = x
+        self.y = y
+    }
 }
